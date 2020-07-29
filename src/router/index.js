20b4c7be54e3980e60b494ba518cc64de9aa55a1
destiny2019 <1457722982@qq.com>
2020-07-29 17:46:38 +0800

    sd

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import HelloWorld from '@/components/HelloWorld'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import Home from "../components/Home";
+import Login from "../components/Login";
 Vue.use(Router)
 Vue.use(ElementUI)
 export default new Router({
@@ -11,8 +12,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home
+      redirect:'/login'
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
